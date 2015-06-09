8c586a3f978da26902b77a8de01ce7ec4dca2a8f
Adrien Gibrat <adrien.gibrat@gmail.com>
2015-06-09 23:05:14 +0200

    only keep mongo notation for comparators & logical operators + various improvements

--- dist/commonjs/mongofilter.js
+++ dist/commonjs/mongofilter.js
@@ -13,25 +13,46 @@ var EXP_LIKE_PERCENT = /(^|[^%])%(?!%)/g // replace unescaped % chars
     EXP_LIKE_UNDERSCORE_REPLACE = function EXP_LIKE_UNDERSCORE_REPLACE(m, p, _) {
 	return p + new Array(_.length + 1).join('.');
 },
+    REGEXP_LIKE = function REGEXP_LIKE(pattern) {
+	return new RegExp('^' + pattern.replace(EXP_LIKE_PERCENT, '$1.*').replace(EXP_LIKE_UNDERSCORE, EXP_LIKE_UNDERSCORE_REPLACE) + '$');
+},
+    EXP_REGEXP = /^\/([\s\S]*)\/([igm]*)$/,
+    REGEXP_PARSE = function REGEXP_PARSE(pattern) {
+	if (typeof pattern === 'string') {
+		(function () {
+			var flag = undefined;
+			pattern.replace(EXP_REGEXP, function (m, e, f) {
+				pattern = e;flag = f;
+			});
+			pattern = flag ? new RegExp(pattern, flag) : new RegExp(pattern);
+		})();
+	}
+	return pattern;
+},
+    EXP_PRIMITIVE = /^(string|number|boolean)$/,
+    IS_PRIMITIVE = function IS_PRIMITIVE(value) {
+	return value == null || EXP_PRIMITIVE.test(typeof value);
+},
     COMPARATORS = {
-	'>': function _(a, b) {
+	$gt: function $gt(a, b) {
 		return a > b;
 	},
-	'>=': function _(a, b) {
+	$gte: function $gte(a, b) {
 		return a >= b;
 	},
-	'<': function _(a, b) {
+	$lt: function $lt(a, b) {
 		return a < b;
 	},
-	'<=': function _(a, b) {
+	$lte: function $lte(a, b) {
 		return a <= b;
 	},
-	'=': function _(a, b) {
+	$eq: function $eq(a, b) {
 		return a === b;
 	},
-	'!=': function _(a, b) {
+	$neq: function $neq(a, b) {
 		return a !== b;
 	},
+<<<<<<< HEAD
 	REGEX: function REGEX(a, b) {
 		if (a === undefined) {
 			return false;
@@ -48,126 +69,90 @@ var EXP_LIKE_PERCENT = /(^|[^%])%(?!%)/g // replace unescaped % chars
 			})();
 		}
 		return !!a.match(b);
+||||||| merged common ancestors
+	REGEX: function REGEX(a, b) {
+		if (typeof b === 'string') {
+			(function () {
+				var flag = undefined,
+				    exp = undefined;
+				b.replace(/^\/([\s\S]*)\/([igm])?/, function (m, e, f) {
+					exp = e;flag = f;
+				});
+				exp || (exp = b);
+				b = flag ? new RegExp(exp, flag) : new RegExp(exp);
+			})();
+		}
+		return !!a.match(b);
+=======
+	$regex: function $regex(a, b) {
+		return REGEXP_PARSE(b).test(a);
+>>>>>>> only keep mongo notation for comparators & logical operators + various improvements
 	},
-	LIKE: function LIKE(a, b) {
-		var exp = new RegExp('^' + b.replace(EXP_LIKE_PERCENT, '$1.*').replace(EXP_LIKE_UNDERSCORE, EXP_LIKE_UNDERSCORE_REPLACE) + '$');
-		return !!((a || a === 0) && a.toString().match(exp));
+	$like: function $like(a, b) {
+		return REGEXP_LIKE(b).test(a);
 	},
-	UNLIKE: function UNLIKE(a, b) {
-		return !COMPARATORS.LIKE(a, b);
+	$nlike: function $nlike(a, b) {
+		return !REGEXP_LIKE(b).test(a);
 	},
-	IN: function IN(a, b) {
+	$in: function $in(a, b) {
 		return !! ~b.indexOf(a);
 	},
-	NOTIN: function NOTIN(a, b) {
+	$nin: function $nin(a, b) {
 		return ! ~b.indexOf(a);
 	}
 },
-    OPERATORS_MAP = {
-	$gt: '>',
-	$gte: '>=',
-	$lt: '<',
-	$lte: '<=',
-	$or: 'OR',
-	'||': 'OR',
-	'$nor': 'NOR',
-	$and: 'AND',
-	'&&': 'AND',
-	$e: '=',
-	$eq: '=',
-	'<>': '!=',
-	$ne: '!=',
-	'!e': '!=',
-	$in: 'IN',
-	'!in': 'NOTIN',
-	$nin: 'NOTIN',
-	'NOT IN': 'NOTIN',
-	$regex: 'REGEX',
-	$like: 'LIKE',
-	$nlike: 'UNLIKE',
-	'!like': 'UNLIKE',
-	'NOT LIKE': 'UNLIKE'
+    LOGICS = {
+	$or: 'some',
+	$nor: 'some',
+	$and: 'every'
+},
+    ALIAS = {
+	$e: '$eq',
+	$ne: '$neq'
 };
 
 /**
- * Handles AND OR and NOR operators
+ * Handles AND, OR and NOR operators
  * @internal
  * @return {boolean}
  */
-function logicalFilter(item, query, operator, property) {
-
-	var res = [];
-
-	if (!Array.isArray(query)) {
-		// first run
-		Object.keys(query).forEach(function (operator) {
-			return res.push(new Mongofilter(query[operator], operator, property).filterItem(item));
-		});
-	} else {
-		// real and or nor
-		query.forEach(function (clause, operator) {
-			return res.push(new Mongofilter(clause, operator, property).filterItem(item));
-		});
-	}
-
-	if (operator === 'OR') {
-		res = !! ~res.indexOf(true);
-	} else if (operator === 'NOR') {
-		res = ! ~res.indexOf(true);
-	} else {
-		res = ! ~res.indexOf(false);
-	}
-
-	return res;
+function logicalOperation(item, query, operator, property) {
+	var result = Array.isArray(query) ? query[LOGICS[operator]](function (query, operator) {
+		return getPredicate(query, operator, property)(item);
+	}) : Object.keys(query)[LOGICS[operator]](function (operator) {
+		return getPredicate(query[operator], operator, property)(item);
+	});
+	return operator === '$nor' ? !result : result;
 }
 
 /**
- * Handles implicit filters ({prop: value} and so on)
- * @return {boolean}
+ * Handles implicit compare ({prop: value} eg. prop === value, etc.)
+ * @param  {Object}  item      collection's item to filter
+ * @param  {*}       query     mongo query descriptor
+ * @param  {String}  property  property name to test against query
+ * @return {boolean}           does item property match query
  */
-function implicitFilter(item, query, property) {
-	var res = true;
-	if ((typeof query).match(/string|number|boolean/)) {
-		res = COMPARATORS['='](item[property], query);
-	} else if (query instanceof Array) {
-		res = COMPARATORS.IN(item[property], query);
-	} else {
-		res = new Mongofilter(query, 'AND', property).filterItem(item);
+function implicitCompare(item, query, property) {
+	if (IS_PRIMITIVE(query)) {
+		return COMPARATORS.$eq(item[property], query);
 	}
-	return res;
+	if (Array.isArray(query)) {
+		return COMPARATORS.$in(item[property], query);
+	}
+	return getPredicate(query, '$and', property)(item);
 }
 
 /**
  * Perform item filtering
- * @param  {*} item     item to filter
- * @param  {*} query    the query descriptor
- * @param  {string} operator current processing operator
- * @param  {string} property the property name to test
- * @return {boolean} whether the item was filtered or not
+ * @param  {*}       query     mongo query descriptor
+ * @param  {String}  operator  logic operator between query root clauses
+ * @param  {String}  property  property name to test against query
+ * @return {Function}          filter predicate function
  */
-function filterItem(item, query, operator, property) {
+function getPredicate(query, _x, property) {
+	var operator = arguments[1] === undefined ? '$and' : arguments[1];
 
-	if (operator in COMPARATORS) {
-		return COMPARATORS[operator](item[property], query);
-	}
-
-	if (operator === 'AND' || operator === 'NOR' || operator === 'OR') {
-		return logicalFilter(item, query, operator, property);
-	}
-
-	// in this case the operator is the property we wana test value against
-	return implicitFilter(item, query, operator);
-}
-
-//-- define a Mongofilter Object --//
-function Mongofilter(clause) {
-	var operator = arguments[1] === undefined ? 'AND' : arguments[1];
-	var propertyName = arguments[2] === undefined ? null : arguments[2];
-
-	// console.log('propertyName', propertyName)
-	OPERATORS_MAP[operator] && (operator = OPERATORS_MAP[operator]);
-
-	this.filterItem = function filterItemMethod(item) {
+	return function (item) {
 		if (typeof item === 'string') {
 			try {
 				item = JSON.parse(item);
@@ -175,33 +160,31 @@ function Mongofilter(clause) {
 				return false;
 			}
 		}
-		return filterItem(item, clause, operator, propertyName);
+		operator = ALIAS[operator] || operator;
+		if (operator in LOGICS) {
+			return logicalOperation(item, query, operator, property);
+		}
+		if (operator in COMPARATORS) {
+			return COMPARATORS[operator](item[property], query);
+		}
+		return implicitCompare(item, query, operator);
 	};
 }
 
-Mongofilter.prototype.filterCollection = function filterCollectionMethod(collection) {
-	if (!collection) {
-		return [];
+function mongofilter(query) {
+	if (typeof query === 'string') {
+		query = JSON.parse(query);
 	}
-	return collection.filter(this.filterItem);
-};
-
-//-- expose the module to the rest of the world --//
-
-function mongofilter(clause) {
-	typeof clause === 'string' && (clause = JSON.parse(clause));
-	if (!clause) {
-		throw 'Invalid clause';
+	if (!query || IS_PRIMITIVE(query)) {
+		throw new TypeError('Invalid query');
 	}
-	var filter = new Mongofilter(clause);
-	var res = function res(item) {
-		return filter.filterItem(item);
+	var predicate = getPredicate(query);
+	predicate.filter = function (collection) {
+		return collection && collection.filter ? collection.filter(predicate) : [];
 	};
-	res.filter = filter.filterCollection;
-	res.filterItem = filter.filterItem;
-	return res;
+	return predicate;
 }
 
-// allow comparators extensibility
+mongofilter.alias = ALIAS;
 mongofilter.comparators = COMPARATORS;
 module.exports = exports['default'];
